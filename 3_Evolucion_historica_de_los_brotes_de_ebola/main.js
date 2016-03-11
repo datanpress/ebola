439a14c03baf4a5ba978e4cbfb5ba328a5c11b29
arnauuc <arnau.udina@gmail.com>
2016-03-11 18:44:49 +0100

    Translation path

--- 3_Evolucion_historica_de_los_brotes_de_ebola/main.js
+++ 3_Evolucion_historica_de_los_brotes_de_ebola/main.js
@@ -4,7 +4,7 @@ if(lang!=null && lang!=""){
   //set the locale in which the messages will be translated
   iJS.i18n.setlocale(lang) ;
   //add domain where to find messages data
-  iJS.i18n.bindtextdomain(lang, "/locale", "po") ;
+  iJS.i18n.bindtextdomain(lang, "../locale", "po") ;
   //Always do this after a `setlocale` or a `bindtextdomain` call.
   iJS.i18n.try_load_lang() ; //will load and parse messages data from the setting catalog.
 }
