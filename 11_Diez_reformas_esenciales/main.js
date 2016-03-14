c3acd1c4898b3a00282eb1cb8c86182341f2263c
arnauuc <arnau.udina@gmail.com>
2016-03-14 15:00:18 +0100

    3

---
+++ 11_Diez_reformas_esenciales/main.js
@@ -0,0 +1,26 @@
+
+var lang = getParameterByName('lang');
+if(lang!=null && lang!=""){
+  //set the locale in which the messages will be translated
+  iJS.i18n.setlocale(lang) ;
+  //add domain where to find messages data
+  iJS.i18n.bindtextdomain(lang, "../locale", "po") ;
+  //Always do this after a `setlocale` or a `bindtextdomain` call.
+  iJS.i18n.try_load_lang() ; //will load and parse messages data from the setting catalog.
+}
+
+
+
+d3.xml("11_nou.svg", "image/svg+xml", function(error, xml) {
+  if (error) throw error;
+  document.body.appendChild(xml.documentElement);
+  //translate
+  // d3.select('.title').text(iJS._("El ébola frente a otras enfermedades en África"));
+  // d3.select('.legend.malaria').text(iJS._("Malaria"));
+  // d3.select('.legend.tuberculosis').text(iJS._("Tuberculosis"));
+  // d3.select('.legend.ebola').text(iJS._("Ébola"));
+  // d3.select('.country.guinea').text(iJS._("Guinea"));
+  // d3.select('.country.sierra_leona').text(iJS._("Sierra Leona"));
+  // d3.select('.country.liberia').text(iJS._("Liberia"));
+
+});
